51ae1a5ec9919f524e28ad6ee4ec12db79640c4f
Henriquehnnm <henriquesardinha987@gmail.com>
2025-08-20 09:15:26 -0300

    Add PDF's

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn input(prompt: &str) -> String {
     println!("{}", prompt);
-    let mut resultado = String::new();
+    let mut resultado: String = String::new();
     io::stdin().read_line(&mut resultado).unwrap();
     resultado.trim().to_string()
 }
@@ -14,7 +14,7 @@ fn main() {
     // Pergunta 1
     println!("\n== Responda as perguntas a seguir: ==\n");
     println!("Em que ano o linux foi criado?\n a) 1992\n b) 1988\n c) 1990\n d) 1991\n");
-    let r1 = input("Responta:");
+    let r1: String = input("Responta:");
     if r1 == "d" {
         println!("\nResposta correta!\n");
         pont += 1.0;
@@ -24,7 +24,7 @@ fn main() {
 
     // Pergunta 2
     println!("O que o comando chmod +x script.sh faz?\n a) Deleta o arquivo\n b) Compila o script\n c) Torna o script executável\n d) Abre o script no editor\n");
-    let r2 = input("Responta:");
+    let r2: String = input("Responta:");
     if r2 == "c" {
         println!("\nResposta correta!\n");
         pont += 1.0;
@@ -34,7 +34,7 @@ fn main() {
 
     // Pergunta 3
     println!("Qual comando é usado para listar todos os arquivos e diretórios, incluindo os ocultos?\n a) ls -l\n b) ls -a\n c) ls -h\n d) ls -r\n");
-    let r3 = input("Responta:");
+    let r3: String = input("Responta:");
     if r3 == "b" {
         println!("\nResposta correta!\n");
         pont += 1.0;
@@ -44,7 +44,7 @@ fn main() {
 
     // Pergunta 4
     println!("Qual diretório contém os arquivos de configuração do sistema no Linux?\n a) /home\n b) /var\n c) /etc\n d) /usr\n");
-    let r4 = input("Responta:");
+    let r4: String = input("Responta:");
     if r4 == "c" {
         println!("\nResposta correta!\n");
         pont += 1.0;
@@ -54,7 +54,7 @@ fn main() {
 
     // Pergunta 5
     println!("Qual comando é usado para procurar por texto dentro de arquivos?\n\na) grep\nb) locate\nc) find\nd) search\n");
-    let r5 = input("Responta:");
+    let r5: String = input("Responta:");
     if r5 == "a" {
         println!("\nResposta correta!\n");
         pont += 1.0;
